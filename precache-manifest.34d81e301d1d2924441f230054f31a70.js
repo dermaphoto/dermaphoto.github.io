155f672e41c2497046af1d1f1e77d399a47ec263
Pratik Chowdhury <pratikc@live.co.uk>
2020-03-17 23:06:47 +0530

    Added Initial Code

---
+++ precache-manifest.34d81e301d1d2924441f230054f31a70.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31319d3679da251e7caa9913a114bab3",
+    "url": "/main_app/index.html"
+  },
+  {
+    "revision": "d30d827ce561b05ecaed",
+    "url": "/main_app/static/css/2.c9d9291a.chunk.css"
+  },
+  {
+    "revision": "4c3c2e96f0e95af5294a",
+    "url": "/main_app/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d30d827ce561b05ecaed",
+    "url": "/main_app/static/js/2.10122997.chunk.js"
+  },
+  {
+    "revision": "22c5a4fdefe00ea92f0c3f14c5a4bdd9",
+    "url": "/main_app/static/js/2.10122997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3c2e96f0e95af5294a",
+    "url": "/main_app/static/js/main.d3a58fbd.chunk.js"
+  },
+  {
+    "revision": "3dd622d2cd4b40a52b75",
+    "url": "/main_app/static/js/runtime-main.58f3dc30.js"
+  },
+  {
+    "revision": "6434d72ce9e16251c4f41f4e5a146567",
+    "url": "/main_app/static/media/female.6434d72c.png"
+  },
+  {
+    "revision": "9210da3d00166247e448b95269f5f1ab",
+    "url": "/main_app/static/media/male.9210da3d.png"
+  }
+]);
